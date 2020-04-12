9885db2d6b73bf77e6979fb581f15b746a32d705
Nihaal Sangha <18350092+OrangutanGaming@users.noreply.github.com>
2020-04-12 17:07:42 +0100

    Added example

---
+++ examples/progress.py
@@ -0,0 +1,15 @@
+import time
+
+from pecho import echo
+
+echo('1', end='%')
+time.sleep(1)
+echo('2%')
+time.sleep(1)
+echo('3%', newline=True)
+time.sleep(1)
+echo('4%')
+time.sleep(1)
+echo()
+time.sleep(1)
+print()
