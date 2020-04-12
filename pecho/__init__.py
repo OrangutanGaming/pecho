5a79c8ba380ac1b267e9f478a790b4bb36401aee
Nihaal Sangha <18350092+OrangutanGaming@users.noreply.github.com>
2020-04-12 17:06:48 +0100

    Improved end and other kwargs

--- pecho/__init__.py
+++ pecho/__init__.py
@@ -30,13 +30,23 @@ __all__ = ['echo']
 __version__ = _version('pecho')
 
 
-def echo(*objects, sep=' ', end='', file=None, flush=True, newline=False, print_func=print, **print_kwargs):
+def echo(*objects, newline=False, end='', pass_end=False, str_convert_func=str, print_func=print, **print_kwargs):
     if objects:
-        objects = ('\r' + str(objects[0]),) + objects[1:]
+        objects = ('\r' + str_convert_func(objects[0]),) + objects[1:]
+    else:
+        objects = ('\r',)
 
     end += '\033[K'
 
     if newline:
         end += '\n'
 
-    print_func(*objects, sep=sep, end=end, file=file, flush=flush, **print_kwargs)
+    force_pass_end = print_func == print
+
+    if pass_end is False or force_pass_end is True:
+        objects = objects[:-1] + (str_convert_func(objects[-1]) + end,)
+        end = ''  # Sets end for print()
+    if pass_end is True or force_pass_end is True:
+        print_kwargs['end'] = end
+
+    return print_func(*objects, **print_kwargs)
--- pecho/__init__.pyi
+++ pecho/__init__.pyi
@@ -1,9 +1,14 @@
-import sys
 from typing import Any, TextIO, Callable, List
 
 __all__: List[str]
 __version__: str
 
-def echo(*objects: Any, sep: str = ' ', end: str = '', file: TextIO = sys.stdout, flush: bool = False,
-         newline: bool = False, print_func: Callable = print, **print_kwargs: Any):
-    ...
+def echo(
+    *objects: Any,
+    newline: bool = False,
+    end: str = '',
+    pass_end: bool = False,
+    str_convert_func: Callable = str,
+    print_func: Callable = print,
+    **print_kwargs: Any,
+): ...
